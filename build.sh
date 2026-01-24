a1872ebebe91128de695e5ddb5486f2849d13ab2
san031 <98796563+san031@users.noreply.github.com>
2026-01-24 11:55:11 +0530

    makemigrations cmd added

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ python manage.py collectstatic --no-input
 
 python manage.py makemigrations --no-input
 
-python manage.py migrate --no-input
+# python manage.py migrate --no-input
+python manage.py migrate base && python manage.py migrate --no-input
